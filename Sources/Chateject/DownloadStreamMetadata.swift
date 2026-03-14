38cd2f332e5ed625fc02707e37b6b8373fef9b22
Lucas Rubio <65956956+soywig@users.noreply.github.com>
2026-03-14 16:20:18 +0100

    refactor: replace force try with safe await in downloadStreamMetadata

--- Sources/Chateject/DownloadStreamMetadata.swift
+++ Sources/Chateject/DownloadStreamMetadata.swift
@@ -18,8 +18,8 @@ public extension Chateject {
         request.setValue(auth.clientId, forHTTPHeaderField: "Client-Id")
         request.setValue("Bearer \(oAuthToken)", forHTTPHeaderField: "Authorization")
         
-        let data = try! await URLSession.shared.data(for: request).0
-        let response = try! JSONDecoder().decode(StreamMetadataResponse.self, from: data)
+        let data = try await URLSession.shared.data(for: request).0
+        let response = try JSONDecoder().decode(StreamMetadataResponse.self, from: data)
         
         return response.data.first
     }
